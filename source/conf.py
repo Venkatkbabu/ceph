44f5226dad940fcdabf72f62f06ef6761523f04a
Venkatkbabu <132256440+Venkatkbabu@users.noreply.github.com>
2026-07-07 13:46:27 +0530

    Add HTML theme side heading

--- source/conf.py
+++ source/conf.py
@@ -29,5 +29,9 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'collapse_navigation': False,
+    'navigation_depth': 4,
+}
 html_static_path = ['_static']
 html_logo = 'logo.png'
